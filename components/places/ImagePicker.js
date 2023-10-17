808ba2d513fd3168323afd62269b93b3f327dac8
Jan Illetško <jan.illetsko@greyson.eu>
2023-10-17 21:20:46 +0200

    initial commit

--- components/places/ImagePicker.js
+++ components/places/ImagePicker.js
@@ -1,14 +1,18 @@
-import { Button, View } from "react-native";
+import { Image, View, Text, StyleSheet } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
 import { Alert } from "react-native";
+import { useState } from "react";
+import Colors from "../../constants/colors";
+import OutlinedButton from "../ui/OutlinedButton";
 
 function ImagePicker() {
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
+  const [pickedImage, setPickedImage] = useState();
   async function verifyPermissions() {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
       const permissionResponse = await requestPermission();
@@ -29,22 +33,49 @@ function ImagePicker() {
     if (!hasPermission) {
       return;
     }
-    const image = await launchCameraAsync({
+    const imageResult = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    if (
+      !imageResult.canceled &&
+      imageResult.assets &&
+      imageResult.assets.length > 0
+    ) {
+      const uri = imageResult.assets[0].uri;
+      setPickedImage(uri);
+    }
+  }
+  let imagePreview = <Text>No image taken yet.</Text>;
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
   return (
     <View>
-      <View></View>
-      <Button
-        title="Take Image"
-        onPress={takeImageHandler}
-      />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 4,
+  },
+});
---
+++ screens/Map.js
@@ -0,0 +1,13 @@
+import MapView, { Marker } from "react-native-maps";
+
+function Map() {
+    const region = {
+        latitude: ,
+        longitude:,
+        latitudeDelta:,
+        longitudeDelta:,
+    }
+  return <MapView initialRegion={}></MapView>;
+}
+
+export default Map;
